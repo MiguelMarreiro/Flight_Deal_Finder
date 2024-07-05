94b82b2163dd98f9cc025be3e1d8c83edb714b66
MiguelMarreiro <miguel.marreiro>
2024-07-05 15:02:45 +0100

    add day range for flight search

--- flight_search.py
+++ flight_search.py
@@ -1,5 +1,5 @@
 import requests
-
+from datetime import datetime, timedelta
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
@@ -10,15 +10,24 @@ class FlightSearch:
         base_url = "https://test.api.amadeus.com/v2"
         url = base_url + "/shopping/flight-offers"
 
+        date = datetime.now()
+
         for destination in destinations:
-            params = {
-                "originLocationCode": origin,
-                "destinationLocationCode": destination,
-                "departureDate": "2024-08-05",
-                "adults": 2,
-                "currencyCode": "EUR",
-                "max": 1
-            }
-            response = requests.get(url=url, headers=header, params=params)
-            response.raise_for_status()
-            print(response.json()["data"][0]["price"])
+            for _ in range(3):
+
+                params = {
+                    "originLocationCode": origin,
+                    "destinationLocationCode": destination,
+                    "departureDate": date.strftime('%Y-%m-%d'),
+                    "adults": 2,
+                    "currencyCode": "EUR",
+                    "max": 1
+                }
+                response = requests.get(url=url, headers=header, params=params)
+                response.raise_for_status()
+                data = response.json()["data"][0]
+                print(date.strftime('%Y-%m-%d'))
+                print(origin + "-" + destination)
+                print(data["price"]["total"] + "€")
+
+                date += timedelta(days=1)
