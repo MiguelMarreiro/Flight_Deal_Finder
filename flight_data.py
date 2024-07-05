6ffce5a969fc28368d7c8c9091444dbd7b5e9ee3
MiguelMarreiro <miguel.marreiro>
2024-07-05 13:49:48 +0100

    token implementation for AMADEUS

--- flight_data.py
+++ flight_data.py
@@ -1,3 +1,22 @@
+import os
+import requests
+
+AMADEUS_ENDPOINT = "https://test.api.amadeus.com/v1"
+
+
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    def __init__(self, token):
+        keywords = ["Paris", "Frankfurt"]
+        #This class is responsible for structuring the flight data.
+        header = {
+            "Authorization": "Bearer " + token
+        }
+        self.city_codes = []
+        for keyword in keywords:
+            params = {
+                "keyword": keyword,
+            }
+            response = requests.get(url=AMADEUS_ENDPOINT+"/reference-data/locations/cities",
+                                    headers=header, params=params)
+            response.raise_for_status()
+            self.city_codes.append(response.json()["data"][0]["iataCode"])
--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import os
+
+import requests
+from flight_data import FlightData
+
+# -------------------------------GETTING TOKEN-------------------------------
+AMADEUS_KEY = os.environ["AMADEUS_KEY"]
+AMADEUS_SECRET = os.environ["AMADEUS_SECRET"]
+AMADEUS_TOKEN = ""
+
+oauth_url = "https://test.api.amadeus.com/v1/security/oauth2/token"
+header = {
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+data = {
+    "grant_type": "client_credentials",
+    "client_id": AMADEUS_KEY,
+    "client_secret": AMADEUS_SECRET,
+}
+
+response = requests.post(url=oauth_url, headers=header, data=data)
+response.raise_for_status()
+AMADEUS_TOKEN = response.json()["access_token"]
+
+# -------------------------------GETTING FlightData-------------------------------
+flight_data = FlightData(AMADEUS_TOKEN)
+print(flight_data.city_codes)
