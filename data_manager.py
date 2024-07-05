1d17da9d98f57dedce54a2e23ee01f74674e0d37
MiguelMarreiro <miguel.marreiro>
2024-07-05 10:36:49 +0100

    connect sheety api and load price data from google sheets

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,11 @@
+import requests
+import os
+
+SHEET_NAME = "prices"
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.sheet_url = os.environ["GOOGLE_SHEET"]
+        self.endpoint = os.environ["SHEETY_ENDPOINT"]
+        self.data = requests.get(url=self.endpoint).json()[SHEET_NAME]
+
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+
+data_manager = DataManager()
+
+# test data_manager
+# print(data_manager.sheet_url, "\n", data_manager.endpoint)
+print(data_manager.data)
